d1436346ed6455a13dd03c7891fdf33e294748b7
yagomalves <yago.melo@souunit.com.br>
2023-10-06 16:32:42 -0300

    removed link from head

--- users.php
+++ users.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Realtime Chat</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudfire.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
 <body>
   <div class="wrapper">
