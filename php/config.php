4375c293265aec4a79a6ddee6309eacb368ed109
Yago Alves <yago.melo@souunit.com.br>
2023-10-06 18:38:07 -0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "Lineage2*", "chat");
+    $conn = mysqli_connect("localhost", "root", "", "chat");
     if(!$conn) {
         echo "Database connected" . mysqli_connect_error();
     }
